cee4b14598cce17fc5f06fe056838fd84a711446
Phillip Gladman <Philgladman@Phillps-MacBook-Pro.local>
2023-01-31 17:16:07 -0500

    update with argocd v2 and test sops

---
+++ sops/create-test-key.sh
@@ -0,0 +1,13 @@
+export KEY_NAME="Phil Test Key"
+export KEY_COMMENT="test key for sops"
+
+gpg --batch --full-generate-key <<EOF
+%no-protection
+Key-Type: 1
+Key-Length: 4096
+Subkey-Type: 1
+Subkey-Length: 4096
+Expire-Date: 0
+Name-Comment: ${KEY_COMMENT}
+Name-Real: ${KEY_NAME}
+EOF
